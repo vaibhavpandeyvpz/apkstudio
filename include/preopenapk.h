b549977bbbbbcb851bc5dc0d7e60967a406be603
Vaibhav Pandey <contact@vaibhavpandey.com>
2015-10-17 13:59:45 +0530

    Removed dex2jar from vendor

--- include/preopenapk.h
+++ include/preopenapk.h
@@ -2,7 +2,6 @@
 #define PREOPENAPK_H
 
 #include <QComboBox>
-#include <QCheckBox>
 #include <QLineEdit>
 #include "dialog.h"
 
@@ -15,14 +14,12 @@ private:
     QLineEdit *_apk;
     QLineEdit *_project;
     QComboBox *_framework;
-    QCheckBox *_sources;
 public:
     explicit PreOpenApk(const QString &apk, QWidget *parent);
 public:
     inline QString apk() { return _apk->text(); }
     QString framework();
     inline QString project() { return _project->text(); }
-    inline bool sources() { return _sources->isChecked(); }
 };
 
 APP_NAMESPACE_END
