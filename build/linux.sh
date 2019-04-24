c891ed6ddd1ffcf69d5fe01f9da6eea0e273dd95
Vaibhav Pandey <contact@vaibhavpandey.com>
2019-04-24 15:38:28 +0530

    Fix download for linuxdeployqt.

--- build/linux.sh
+++ build/linux.sh
@@ -8,10 +8,8 @@ qmake CONFIG+=release PREFIX=/usr $TRAVIS_BUILD_DIR
 make
 make install INSTALL_ROOT=AppDir
 
-if [ -f linuxdeployqt*.AppImage ]; then
-  wget -c -nv https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage
-fi
-chmod +x linuxdeploy*.AppImage
+wget -c -nv https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage
+chmod +x linuxdeployqt*.AppImage
 unset QTDIR; unset QT_PLUGIN_PATH; unset LD_LIBRARY_PATH
 mkdir -p AppDir/usr/lib/
 cp -r /usr/lib/x86_64-linux-gnu/nss AppDir/usr/lib/
