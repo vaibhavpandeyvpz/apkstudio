d8cc2d0d02217803ef326cc73a97cac88a11e7dd
Vaibhav Pandey <contact@vaibhavpandey.com>
2025-12-04 13:31:26 +0530

    Decompile APK from anywhere

--- sources/splashwindow.h
+++ sources/splashwindow.h
@@ -9,10 +9,11 @@ class SplashWindow : public QMainWindow
 {
     Q_OBJECT
 public:
-    SplashWindow();
+    explicit SplashWindow(const QString &apkFilePath = QString());
     ~SplashWindow();
 private:
     QMap<QString, QString> m_Versions;
+    QString m_ApkFilePath;
     QWidget *buildCentralWidget();
 private slots:
     void handleVersionResolved(const QString &binary, const QString &version);
